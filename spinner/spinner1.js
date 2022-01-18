41a7169ffbafccea3c4459a60022b84078defe8d
Fransiskus <aldzaidea@gmail.com>
2022-01-18 19:17:32 +0000

    add spinner folder and create spinner animation file

---
+++ spinner/spinner1.js
@@ -0,0 +1,67 @@
+setTimeout(() => {
+  process.stdout.write("\r|  ");
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write("\r/  ");
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write("\r-  ");
+}, 500);
+
+setTimeout(() => {
+  process.stdout.write("\r\\  ");
+}, 700);
+
+setTimeout(() => {
+  process.stdout.write("\r|  ");
+}, 1000);
+
+setTimeout(() => {
+  process.stdout.write("\r/  ");
+}, 1200);
+
+setTimeout(() => {
+  process.stdout.write("\r-  ");
+}, 1400);
+
+setTimeout(() => {
+  process.stdout.write("\r\\  ");
+}, 1600);
+
+setTimeout(() => {
+  process.stdout.write("\r|  ");
+}, 1800);
+
+setTimeout(() => {
+  process.stdout.write("\r/  ");
+}, 2000);
+
+setTimeout(() => {
+  process.stdout.write("\r-  ");
+}, 2200);
+
+setTimeout(() => {
+  process.stdout.write("\r\\  ");
+}, 2500);
+
+setTimeout(() => {
+  process.stdout.write("\r|  ");
+}, 2700);
+
+setTimeout(() => {
+  process.stdout.write("\r/  ");
+}, 2900);
+
+setTimeout(() => {
+  process.stdout.write("\r-  ");
+}, 3100);
+
+setTimeout(() => {
+  process.stdout.write("\r\\  ");
+}, 3300);
+
+setTimeout(() => {
+  process.stdout.write("\r|  ");
+}, 3500);
